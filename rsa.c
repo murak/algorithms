b90446bef6e5e71bd5c99854ae45626a6fc14ae1
murak <muralik.msr@gmail.com>
2013-04-02 09:28:54 +0530

    adding RSA algorithms

---
+++ rsa.c
@@ -0,0 +1,77 @@
+/* C program for the Implementation Of RSA Algorithm */
+
+#include<stdio.h>
+
+int phy,P,n,e,d,C,FLAG;
+
+int check() {
+    int i;
+    for(i=3;e%i==0 && phy%i==0;i+2)
+    {
+        FLAG = 1;
+        return;
+    }
+    FLAG = 0;
+}
+
+void encrypt() {
+    int i;
+    C = 1;
+    for(i=0;i< e;i++)
+        C=C*P%n;
+    C = C%n;
+    printf("\n\tAfter Encryption. Cipher Text : %d\n\n",C);
+}
+
+void decrypt() {
+    int i;
+    P = 1;
+    for(i=0;i< d;i++)
+        P=P*C%n;
+    P = P%n;
+    printf("\n\tAfter Decrption. Plain Text : %d\n\n",P);
+}
+
+void main() {
+    int p,q,s;
+    char ch= '3';
+    printf("Enter Two Relatively Prime Numbers\t: ");
+    scanf("%d%d",&p,&q);
+    n = p*q;
+    phy=(p-1)*(q-1);
+    printf("\nphy(n)\t= %d",phy);
+    do
+    {
+        printf("\nEnter value of e\t: ");
+        scanf("%d",&e);
+        check();
+    } while(FLAG==1);
+    d = 1;
+    do
+    {
+        s = (d*e)%phy;
+        d++;
+    } while(s!=1);
+    d = d-1;
+    printf("\n\tPublic Key\t: {%d,%d}",e,n);
+    printf("\n\tPrivate Key\t: {%d,%d}",d,n);
+    
+    printf("\n\nPress\n1. Encrypting \n2. Decrypting\t");
+    scanf("%c%c",&ch,&ch);
+    switch(ch) {
+        case '1': 
+                printf("\n\nEnter The Plain Text\t: ");
+                scanf("%d",&P);
+                encrypt();
+                break;
+        case '2': 
+                printf("\n\nEnter the Cipher text\t: ");
+                scanf("%d",&C);
+                decrypt();
+                break;
+        default: 
+                printf("\nInvalid Option!!\n");
+                    
+    }
+
+}
