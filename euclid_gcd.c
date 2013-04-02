5dc516c7e62480e6096c34bdec4d164a7dd85e6f
murak <muralik.msr@gmail.com>
2013-04-02 08:55:24 +0530

    adding extended euclid, gcd, modular inverse, power mod algorithms

---
+++ euclid_gcd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("\nUsage: %s <num1> <num2> \n",argv[0]);
+        return 0;
+    }
+    printf("\ngcd of %s and %s is = %d\n",
+            argv[1], argv[2], gcd(atoi(argv[1]), atoi(argv[2])));
+    return 0;
+}
